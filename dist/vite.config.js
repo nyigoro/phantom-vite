c16648847fe278074f17e0bb0dc1a40e404b9ba5
nyigoro <nyigoro@gmail.com>
2025-06-27 01:11:08 +0300

    Update vite.config.js

--- dist/vite.config.js
+++ dist/vite.config.js
@@ -1,1 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'scripts/example.ts',
+      name: 'ExampleScript',
+      fileName: 'example',
+      formats: ['es']
+    },
+    outDir: 'dist',
+    emptyOutDir: false,
+    minify: false
+  }
+})
 
